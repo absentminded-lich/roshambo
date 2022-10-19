60ddb0a3b9c0ede73077ccceedee7312d7d9264c
Brett Parker <absentminded.lich@gmail.com>
2022-10-19 15:17:09 -0400

    Add string to int and change various names

--- roshambo.js
+++ roshambo.js
@@ -2,13 +2,29 @@
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
+// return string corresponding int
+function getIntChoice(strChoice) {
+    switch(strChoice.toLowerCase()) {
+        case 'p':
+        case 'paper':
+            return 0;
+        case 's':
+        case 'scissors':
+            return 1;
+        case 'r':
+        case 'rock': 
+            return 2;
+        default:
+            return undefined;
+    }
+}
 // return int corresponding string
-function displayChoice(numChoice) {
-    switch(numChoice) {
+function getStrChoice(intChoice) {
+    switch(intChoice) {
         case 0: return 'Paper';
         case 1: return 'Scissors';
         case 2: return 'Rock';
-        default: return 'N/A';
+        default: return undefined;
     }
 }
 // process outcome and return as string message
@@ -18,14 +34,14 @@ function playRound(playerSelection, computerSelection) {
         return 'A Tie! An interesting declaration beats this generic one.';
     } else if (playerSelection > computerSelection || (playerSelection === 2 && computerSelection == 0)) {
         // player wins
-        return 'You Win! ' + displayChoice(playerSelection) + ' beats ' + displayChoice(computerSelection);
+        return 'You Win! ' + getStrChoice(playerSelection) + ' beats ' + getStrChoice(computerSelection);
     } else {
         // player loses
-        return 'You Lose! ' + displayChoice(computerSelection) + ' beats ' + displayChoice(playerSelection);
+        return 'You Lose! ' + getStrChoice(computerSelection) + ' beats ' + getStrChoice(playerSelection);
     }
 }
 
-let playerSelection = 1; // rock
+let playerSelection = 0; // rock
 let computerSelection = getComputerChoice();
 
 console.log(playRound(playerSelection, computerSelection));
