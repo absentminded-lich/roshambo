e90c58050a12360ef417e85f55566b3d372ce57d
Brett Parker <absentminded.lich@gmail.com>
2022-10-19 14:12:48 -0400

    Add playRound() and decouple some

--- roshambo.js
+++ roshambo.js
@@ -1,11 +1,31 @@
+// return rand int between 1 and 3
 function getComputerChoice() {
-    let rng = Math.floor(Math.random() * 3);
-    switch(rng) {
-        case 0: return 'Rock';
+    return Math.floor(Math.random() * 3);
+}
+// return int corresponding string
+function displayChoice(numChoice) {
+    switch(numChoice) {
+        case 0: return 'Paper';
         case 1: return 'Scissors';
-        case 2: return 'Paper';
-        default: return 'How are you here?!';
+        case 2: return 'Rock';
+        default: return 'N/A';
+    }
+}
+// process outcome and return as string message
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        // player ties
+        return 'A Tie! An interesting declaration beats this generic one.';
+    } else if (playerSelection > computerSelection || (playerSelection === 2 && computerSelection == 0)) {
+        // player wins
+        return 'You Win! ' + displayChoice(playerSelection) + ' beats ' + displayChoice(computerSelection);
+    } else {
+        // player loses
+        return 'You Lose! ' + displayChoice(computerSelection) + ' beats ' + displayChoice(playerSelection);
     }
 }
 
-console.log(getComputerChoice());
+let playerSelection = 1; // rock
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
