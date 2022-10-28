2a0a8145e39d64cd8253159869eda7b970b4c80e
Brett Parker <absentminded.lich@gmail.com>
2022-10-27 22:34:33 -0400

    Lose yourself in a variety of changes

--- roshambo.js
+++ roshambo.js
@@ -1,74 +1,74 @@
-// return a random int between 0 and maxRange
-function getEnemyChoice(maxRange = 3) {
+function generateRndVerb(withSpaces = true) {
+    const verbs = ['slays', 'beats', 'conquers', 'overpowers', 'destroys', 'overwhelms', 'massacres', 'butchers',
+        'zaps', 'erases', 'slaughters', 'kills', 'trounces', 'pulverizes', 'demolishes', 'annihilates', 'obliterates',
+        'eliminates', 'exterminates', 'eradicates', 'liquidates', 'vanquishes', 'subdues', 'clobbers', 'quashes'];
+    return (withSpaces ? ' ' : '') + verbs[Math.floor(Math.random() * verbs.length)].toUpperCase() + (withSpaces ? ' ' : '');
+}
+
+function getCPUSelection(maxRange = 3) {
     return Math.floor(Math.random() * maxRange);
 }
-// prompt the player for maxRounds and return that value
-function getMaxRounds(msg, initVal = 5) {
-    let maxRounds;
 
-    while (true) {
-        maxRounds = prompt(msg, initVal);
-        if (maxRounds === null) return null;
-        if (!isNaN(maxRounds)) {
-            if(maxRounds <= 0) continue;
-            return maxRounds;
+function incPlayerScore(inc = 1) {
+    playerScore += inc;
+    document.querySelector('#player-score').textContent = playerScore;
+}
+
+function incCPUScore(inc = 1) {
+    cpuScore += inc;
+    document.querySelector('#cpu-score').textContent = cpuScore;
+}
+
+function outMsg(msg) {
+    document.querySelector('#output').textContent = msg;
+    console.log(msg);
+}
+
+function playRound(playerChoice) {
+    let playerSelection = choices.findIndex(choice => playerChoice === choice);
+
+    let cpuSelection = getCPUSelection();
+
+    if (playerSelection === cpuSelection) {
+        // player ties round
+        outMsg('BALANCED');
+    } else if (playerSelection > cpuSelection || (playerSelection == 0 && cpuSelection == 2)) {
+        // player wins round
+        outMsg(choices[playerSelection].toUpperCase() + generateRndVerb() + choices[cpuSelection].toUpperCase());
+        incPlayerScore();
+        if (playerScore >= minRounds) {
+            outMsg('VICTORY');
         }
-    }
-}
-// prompt the player for a specific value and return that value
-function getPlayerChoice(msg, choices) {
-    let playerChoice;
-
-    while (true) {
-        playerChoice = prompt(msg);
-        if (playerChoice === null) return null;
-        let test = choices.findIndex(n => n.toLowerCase() === playerChoice.toLowerCase() || n.charAt(0).toLowerCase() == playerChoice.toLowerCase());
-        if (test !== -1) return test;
-    }
-}
-// game loop
-function game() {
-    let playerChoice, enemyChoice;
-    let playerWins = 0;
-    let enemyWins = 0;
-
-    const choices = ['Rock', 'Paper', 'Scissors'];
-
-    let maxRounds = getMaxRounds("Best out of?");
-    if (maxRounds === null) return; // cancel
-
-    let roundsToWin = Math.ceil((maxRounds / 2) + 0.01);
-
-    for (let round = 1; round <= maxRounds; round++) {
-        // player input
-        playerChoice = getPlayerChoice(`[R]ock, [P]aper, or [S]cissors? (Round ${round} of ${maxRounds})`, choices);
-        if (playerChoice === null) return; // cancel
-        // computer "input"
-        enemyChoice = getEnemyChoice(choices.length);
-
-        if (playerChoice === enemyChoice) {
-            // player ties round
-            console.log('A Tie! Try again!');
-            round--;
-            continue;
-        } else if (playerChoice > enemyChoice || (playerChoice == 0 && enemyChoice == 2)) {
-            // player wins round
-            console.log(`You Win! ${choices[playerChoice]} beats ${choices[enemyChoice]} [Wins: ${++playerWins}]`);
-            if (playerWins >= roundsToWin) {
-                // player wins game
-                console.log(`Congratulations! You have won ${playerWins} out of ${maxRounds} rounds!`);
-                return;
-            }
-        } else {
-            // player loses round
-            console.log(`You Lose! ${choices[enemyChoice]} beats ${choices[playerChoice]}  [Losses: ${++enemyWins}]`);
-            if (enemyWins >= roundsToWin) {
-                // player loses game
-                console.log(`Defeat! The computer has won ${enemyWins} out of ${maxRounds} rounds!`);
-                return;
-            }
+    } else {
+        // player loses round
+        outMsg(choices[cpuSelection].toUpperCase() + generateRndVerb() + choices[playerSelection].toUpperCase());
+        incCPUScore();
+        if (cpuScore >= minRounds) {
+            outMsg('DEFEAT');
         }
     }
 }
 
-game();
+let cpuScore = 0;
+let maxRounds = 9;
+let playerScore = 0;
+
+const choices = ['rock', 'paper', 'scissors'];
+
+let minRounds = Math.ceil((maxRounds / 2) + 0.01);
+
+const btns = document.querySelectorAll('button');
+btns.forEach(btn => {
+    btn.addEventListener('click', () => playRound(btn.id));
+    btn.addEventListener('mousedown', () => {
+        btn.style.background = 'white';
+        btn.style.color = 'black';
+    });
+    btn.addEventListener('mouseup', () => {
+        btn.style.background = 'black';
+        btn.style.color = 'white';
+    });
+});
+
+const scores = document.querySelectorAll('.score');
+scores.forEach(score => score.textContent = 0);
